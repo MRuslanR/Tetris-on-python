f96354d72004f40cee1351c77779f43562a69f61
MRuslanR <rrmukhametshin@ede.hse.ru>
2023-10-18 13:51:00 +0300

    Размечена сетка игрого поля + созданы все 7 всевозможных фигур

--- main.py
+++ main.py
@@ -25,16 +25,38 @@ screen=pygame.display.set_mode((W*SQUARE,H*SQUARE))
 icon = pygame.image.load('images/icon1.jpg')
 pygame.display.set_icon(icon)
 
+#Создание сетки поля, rect() отвечает за координаты квадрата
+grid = [pygame.Rect(x*SQUARE,y*SQUARE,SQUARE,SQUARE) for x in range (W) for y  in range(H)]
 
+#Создание всех 7 фигур. 1 координата в каждом из 7 подмассивов отвечает за центр вращения фигуры
+figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
+               [(0, -1), (-1, -1), (-1, 0), (0, 0)],
+               [(-1, 0), (-1, 1), (0, 0), (0, -1)],
+               [(0, 0), (-1, 0), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, -1)],
+               [(0, 0), (0, -1), (0, 1), (1, -1)],
+               [(0, 0), (0, -1), (0, 1), (-1, 0)]]
+
+figures = [[pygame.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
+figure_rect = pygame.Rect(0, 0, SQUARE - 2, SQUARE - 2)
+
+figure=figures[3]
 while True:
     #Цвет игрового поля
-    screen.fill(pygame.Color('red'))
+    screen.fill(pygame.Color('black'))
 
     #Выход из программы при закрытии окна
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             exit()
+    #Отрисовка сетки поля
+    [pygame.draw.rect(screen, ("white"), i_rect, 1) for i_rect in grid]
+
+    for i in range(4):
+        figure_rect.x=figure[i].x*SQUARE
+        figure_rect.y = figure[i].y * SQUARE
+        pygame.draw.rect(screen,('red'),figure_rect)
 
     #Обновление игрового экрана
-    pygame.display.update()
+    pygame.display.flip()
     clock.tick(FPS)
