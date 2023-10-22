4afc44d2ef22bc014e521ae57304031c48f7a047
MRuslanR <rrmukhametshin@ede.hse.ru>
2023-10-22 17:22:57 +0300

    added finish line + fixed 5

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ import copy
 
 pygame.init()
 
-#Разрешение игры
-RESOLUTION = 0
 #ФПС
-FPS=60
+FPS=120
 #Количество квадратиков-полей (горизонталь, вертикаль)
 W, H = 10, 15
 #Размеры квадратиков-поля
 SQUARE=45
-
 
 #Название игры
 pygame.display.set_caption("Tetris from КНТ")
@@ -43,23 +40,31 @@ figures = [[pygame.Rect(x + W//2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos 
 #Объект, отвечающий за отрисовку квадратика. -2 для того, чтобы квадратик не перерисовывал стенки
 figure_rect = pygame.Rect(0, 0, SQUARE - 2, SQUARE - 2)
 
-figure=figures[random.randint(0,6)]
+figure=copy.deepcopy(random.choice(figures))
 
 #Функция проеврки выхода за границу W
-def check_borders(cur):
-    if cur < 0 or cur > W - 1:
+
+#cur = figure[i]
+def check_borders():
+    if figure[i].x < 0 or figure[i].x > W - 1:
+        return False
+    elif figure[i].y > H - 1 or field[figure[i].y][figure[i].x]:
         return False
     return True
 
-animation_speed=80
+animation_speed=40
 animation_limit=2400
 animation_current=0
+#Игровое поле, в котором будут отмечаться уже упавшие фигуры
+field = [[0 for i in range(W)] for j in range(H+1)]
+
+figure = figures[1]
 
 while True:
     #Цвет игрового поля
     screen.fill(pygame.Color('black'))
-
     dx = 0
+    flag_rotate = False
 
     #Выход из программы при закрытии окна
     for event in pygame.event.get():
@@ -73,24 +78,50 @@ while True:
                 dx+=1
             if event.key==pygame.K_DOWN:
                 animation_limit=160
+            if event.key==pygame.K_UP:
+                flag_rotate = True
 
-    #Перемещение всех 4 квадратиков на dx c проверкой выхода за границу
-    figure_old = copy.deepcopy(figure)
-    for i in range(4):
-        figure[i].x += dx
-        if not check_borders(figure[i].x):
-            figure = copy.deepcopy(figure_old)
-            break
+
     #Отрисовка сетки поля
     [pygame.draw.rect(screen, ("white"), i_rect, 1) for i_rect in grid]
+
 
     #Сдвиг фигуры вниз на 1 единицу при превышении лимита анимации
     animation_current+=animation_speed
     if animation_current>animation_limit:
         animation_current=0
+        figure_old=copy.deepcopy(figure)
         for i in range(4):
             figure[i].y+=1
+            if not check_borders():
+                for i in range (4):
+                    print(figure_old[i],figure[i])
+                    field[figure_old[i].y][figure_old[i].x] = pygame.Color('red') #mfidgjdjiejg
+                figure=copy.deepcopy(random.choice(figures))
+                animation_limit = 2400
+                break
 
+    #Перемещение всех 4 квадратиков на dx c проверкой выхода за границу
+    figure_old = copy.deepcopy(figure)
+    for i in range(4):
+        figure[i].x += dx
+        if not check_borders():
+            figure = copy.deepcopy(figure_old)
+            break
+
+
+    # переворот фигуры
+    if flag_rotate == True and figures[1]!=figure:
+        center = figure[0]
+        figure_old = copy.deepcopy(figure)
+        for i in range(4):
+            x = figure[i].y - center.y
+            y = figure[i].x - center.x
+            figure[i].x = center.x - x
+            figure[i].y = center.y + y
+            if not check_borders():
+                figure = copy.deepcopy(figure_old)
+                break
 
     #Отрисовка фигуры путем рисования 4-х квадратиков
     for i in range(4):
@@ -98,9 +129,24 @@ while True:
         figure_rect.y = figure[i].y * SQUARE
         pygame.draw.rect(screen,('red'),figure_rect)
 
+    #Проверка заполненности линии
 
+    for i in range (H-1,0,-1):
+        if all(field[i][j]!=0 for j in range (W)):
+            for j in range(i,0,-1):
+                field[j]=copy.deepcopy(field[j-1])
+
+
+
+ #IODIWHDHWIDHIWHD
+    for y, raw in enumerate(field):
+        for x, col in enumerate(raw):
+            if col:
+                figure_rect.x, figure_rect.y = x * SQUARE, y * SQUARE
+                pygame.draw.rect(screen, col, figure_rect)
 
 
     #Обновление игрового экрана
     pygame.display.flip()
     clock.tick(FPS)
+
